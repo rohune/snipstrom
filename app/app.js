87129f9bcc8c9fdeae8cf01420bec287133b2565
rohune <psdrama@gmail.com>
2015-01-17 09:20:11 +0200

    noniin

--- app/app.js
+++ app/app.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var app = express();
 
+var respond = function(request, response) {
+  if (request.query.url) {
+    response.send(request.query.url);
+  } else {
+    response.send('hello yliseppo!');  
+  }
+}
+
 app.get('/', function(request, response) {
- response.send('hello yliseppo!');
+  response.send('please use /api');
+}
+
+app.get('/api', function(request, response) {
+  respond(request, response);
+});
+
+app.post('/api/shorten', function(request, response) {
+  
 });
 
 app.listen(3000, function() {
