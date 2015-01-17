3848b81cf4fa703306ee39698d6fcb553770502e
rohune <psdrama@gmail.com>
2015-01-17 08:24:16 +0200

    /usr/bin/gcm

---
+++ app/app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(request, response) {
+ response.send('hello yliseppo!');
+});
+
+app.listen(3000, function() {
+ console.log('yliseppo is listening on 3000...');
+});
