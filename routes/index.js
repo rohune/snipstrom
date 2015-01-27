e4b25b106ab067acdc10920873009397b0f63154
rohune <psdrama@gmail.com>
2015-01-27 12:55:53 +0200

    simple index page behaviour

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var urldb = require('../urldb');
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'welcome to yliseppo!' });
+  var title = "snipstrom -- url shortener";
+  var urls = urldb.getUrls();
+  
+  res.render('index', { title: title, urls: urls });
 });
 
 module.exports = router;
