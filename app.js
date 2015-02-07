002467cdee8c6bf5a23391e501af4f6d9a688134
Jaakko Rohunen <jaakko.rohunen@gmail.com>
2015-02-07 10:58:40 +0200

    remove dependency, add config

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var echo = require('./routes/echo');
 var shorten = require('./routes/shorten');
 var id = require('./routes/id');
 
@@ -21,11 +19,9 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/echo', echo);
 app.use('/shorten', shorten);
 app.use('/id', id);
 
---
+++ config.js
@@ -0,0 +1,5 @@
+exports.config = {
+  "rootUrl": "http://snip.yliseppo.com",
+  "randomLength": 4,
+  "validChars": "abcdefghijklmnopqrstuvwxyz"
+}
